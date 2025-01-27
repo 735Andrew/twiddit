9d9757c0cd3ea055b15863fe5770be4143c89238
Andrew735 <example@example.com>
2025-01-27 14:15:58 +0300

    Small mistake fix

--- twiddit.py
+++ twiddit.py
@@ -3,7 +3,6 @@ import sqlalchemy.orm as so
 from app import db, create_app
 from app.models import User,Post
 app = create_app()
-t   
 @app.shell_context_processor
 def hello():
     return {"sa":sa, "so":so, "db":db, "User":User, "Post":Post}
