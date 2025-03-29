38bf4542b5a8dcc974675e63c52fd6142907aaba
735Andrew <kva6519@gmail.com>
2025-03-29 10:51:08 +0300

    API

--- app/models.py
+++ app/models.py
@@ -1,5 +1,5 @@
 from werkzeug.security import generate_password_hash, check_password_hash
-from datetime import datetime, timezone
+from datetime import datetime, timezone, timedelta
 from typing import Optional
 import sqlalchemy as sa
 import sqlalchemy.orm as so
@@ -7,8 +7,8 @@ from app import db, login
 from flask_login import UserMixin
 from hashlib import md5
 from time import time
-import jwt, json, rq, redis
-from flask import current_app
+import jwt, json, rq, redis, secrets
+from flask import current_app, url_for
 
 followers = sa.Table(
     "followers",
@@ -18,7 +18,30 @@ followers = sa.Table(
 )
 
 
-class User(UserMixin, db.Model):
+class PaginatedAPIMixin(object):
+    @staticmethod
+    def to_collection_dict(query, page, per_page, endpoint, **kwargs):
+        resources = db.paginate(query, page=page, per_page=per_page, error_out=False)
+        data = {
+            "items": [item.to_dict() for item in resources.items],
+            "meta": {
+                "page": page,
+                "per_page": per_page,
+                "total_pages": resources.pages,
+                "total_items": resources.total
+            },
+            "_links": {
+                "self": url_for(endpoint, page=page, per_page=per_page, **kwargs),
+                "next": url_for(endpoint, page=page + 1, per_page=per_page, **kwargs) if \
+                    resources.has_next else None,
+                "prev": url_for(endpoint, page=page - 1, per_page=per_page, **kwargs) if \
+                    resources.has_prev else None
+            }
+        }
+        return data
+
+
+class User(PaginatedAPIMixin, UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True, unique=True)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True)
@@ -51,6 +74,9 @@ class User(UserMixin, db.Model):
                                                                        back_populates="recipient")
 
     tasks: so.WriteOnlyMapped["Task"] = so.relationship(back_populates="user")
+
+    token: so.Mapped[Optional[str]] = so.mapped_column(sa.String(32), index=True, unique=True)
+    token_expiration: so.Mapped[Optional[datetime]]
 
     def add_notification(self, name, data):
         db.session.execute(self.notifications.delete().where(Notification.name == name))
@@ -140,6 +166,59 @@ class User(UserMixin, db.Model):
     def get_task_in_progress(self, name):
         query = self.tasks.select().where(Task.name == name, Task.complete == False)
         return db.session.scalar(query)
+
+    def posts_count(self):
+        query = sa.select(sa.func.count()).select_from(self.posts.select().subquery())
+        return db.session.scalar(query)
+
+    def to_dict(self, include_email=False):
+        data = {
+            "id": self.id,
+            "username": self.username,
+            "last_seen": self.last_seen.replace(
+                tzinfo=timezone.utc).isoformat() if self.last_seen else None,
+            "about_me": self.about_me,
+            "post_count": self.posts_count(),
+            "follower_count": self.followers_count(),
+            "following_count": self.following_count(),
+            "_links": {
+                "self": url_for("api.get_user", id=self.id),
+                "followers": url_for("api.get_followers", id=self.id),
+                "following": url_for("api.get_following", id=self.id),
+                "avatar": self.avatar(128)
+            }
+        }
+        if include_email:
+            data["email"] = self.email
+
+        return data
+
+    def from_dict(self, data, new_user=False):
+        for field in ["username", "email", "about_me"]:
+            if field in data:
+                setattr(self, field, data[field])
+        if new_user and "password" in data:
+            self.set_password(data["password"])
+
+    def get_token(self, expires_in=3600):
+        now = datetime.now(timezone.utc)
+        if self.token and self.token_expiration.replace(tzinfo=timezone.utc) > now + timedelta(seconds=60):
+            return self.token
+        self.token = secrets.token_hex(16)
+        self.token_expiration = now + timedelta(seconds=expires_in)
+        db.session.add(self)
+        return self.token
+
+    def revoke_token(self):
+        self.token_expiration = datetime.now(timezone.utc) - timedelta(seconds=1)
+
+    @staticmethod
+    def check_token(token):
+        user = db.session.scalar(sa.select(User).where(User.token == token))
+        if user is None or user.token_expiration.replace(tzinfo=timezone.utc) < \
+                datetime.now(timezone.utc):
+            return None
+        return user
 
 
 class Post(db.Model):
